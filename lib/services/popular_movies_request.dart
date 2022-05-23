4dfcfc10301df301779014601fa64b59a16ffa10
Muhammed AKAR <99355169+muhammedakar@users.noreply.github.com>
2022-05-23 15:34:48 +0300

    Update popular_movies_request.dart

--- lib/services/popular_movies_request.dart
+++ lib/services/popular_movies_request.dart
@@ -2,8 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:movies/products/popular_movies.dart';
 
 class PopularRequest {
+  String apiToken= ""; // you can take api-key in this link https://www.themoviedb.org/
   String url(String category) =>
-      "https://api.themoviedb.org/3/movie/$category?{api-Key}=tr"; // you can take api-key in this link https://www.themoviedb.org/
+      "https://api.themoviedb.org/3/movie/$category?$apiToken=tr"; 
 
   Future<List<PopularMovies>> moviesList(String category) async {
     Response response = await Dio().get(url(category));
